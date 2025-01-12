3613ea03768e983f4aaa9c7a9412c7878a8e67fe
Alejandro Garcia Alonso <alejo_2986@hotmail.com>
2025-01-12 16:32:07 -0600

    chore: Remove unnecessary semicolons and fix import statements in GamePage.jsx

--- src/Routes/Game/GamePage.jsx
+++ src/Routes/Game/GamePage.jsx
@@ -1,36 +1,36 @@
-import { Header } from '../../components/Header/header';
-import { SectionGame } from '../../components/SectionGame/SectionGame';
-import './App.css';
-import { SecretWord } from '../../components/SecretWord/SecretWord';
-import { Keyboard } from '../../components/Keyboard/Keyboard';
-import { Footer } from '../../components/Footer/Footer';
-import { HumanBody } from '../../components/HumanBody/HumanBody';
-import { useStats } from '../../Hooks/UseStats';
-import { Clues } from '../../components/Clues/Clues';
-import { useGlobal } from "../../Hooks/UseGlobal";
-import { usePokemonData } from '../../Hooks/UsePokemonData';
-import { Modal } from '../../components/Modal/Modal';
-import { useState } from 'react';
+import { Header } from '../../components/Header/header'
+import { SectionGame } from '../../components/SectionGame/SectionGame'
+import './App.css'
+import { SecretWord } from '../../components/SecretWord/SecretWord'
+import { Keyboard } from '../../components/Keyboard/Keyboard'
+import { Footer } from '../../components/Footer/Footer'
+import { HumanBody } from '../../components/HumanBody/HumanBody'
+import { useStats } from '../../Hooks/UseStats'
+import { Clues } from '../../components/Clues/Clues'
+import { useGlobal } from "../../Hooks/UseGlobal"
+import { usePokemonData } from '../../Hooks/UsePokemonData'
+import { Modal } from '../../components/Modal/Modal'
+import { useState } from 'react'
 
 function GamePage() {
-  const { stateStats, updateAttemp, updateLetter } = useStats();
-  const { stateLives, setStateLives, winner, setWinner } = useGlobal();
-  const { pokemonData } = usePokemonData();
-  const [viewModal, setViewModal] = useState(false);
-  const [viewClue, setViewClue] = useState(false);
+  const { stateStats, updateAttemp, updateLetter } = useStats()
+  const { stateLives, setStateLives, winner, setWinner } = useGlobal()
+  const { pokemonData } = usePokemonData()
+  const [viewModal, setViewModal] = useState(false)
+  const [viewClue, setViewClue] = useState(false)
 
 
   if (stateLives < 1) {
     setTimeout(() => {
-      setViewModal(true);
-    }, 1000);
+      setViewModal(true)
+    }, 1000)
   } else if (winner) {
     setTimeout(() => {
-      setViewModal(true);
-    }, 400);
-  } 
-  
-  
+      setViewModal(true)
+    }, 400)
+  }
+
+
   return (
     <>
       <Header ></Header>
@@ -41,7 +41,7 @@ function GamePage() {
           pokemonData={pokemonData}
         ></SecretWord>
         <HumanBody stateLives={stateLives}></HumanBody>
-        <Clues stateLives={stateLives} setViewClue={setViewClue}  setStateLives={setStateLives}></Clues>
+        <Clues stateLives={stateLives} setViewClue={setViewClue} setStateLives={setStateLives}></Clues>
       </SectionGame>
       <Footer>
         <Keyboard updateLetter={updateLetter} setStateLives={setStateLives} stateLives={stateLives} pokemonData={pokemonData} setWinner={setWinner}></Keyboard>
